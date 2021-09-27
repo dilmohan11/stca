1252e42aff0260c9c6e96ad4de2a3b72a96abaf0
Dilmohan <dilmohan11@gmail.com>
2021-09-27 13:35:44 +0530

    Added Tailwind

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About Us</router-link> |
-    <router-link to="/contact">Contact us</router-link> |
-    <router-link to="/register">Register</router-link>
+    <Navbar />
+    
   </div>
   <router-view/>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+    components: { Navbar }
+}
+</script>
+
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -28,5 +34,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+<header class="navbar h-24 flex justify-between text-white font-bold text-xl">
+
+  <img src="../assets/images/logo.png" alt="" class="">
+  <!-- <div class="">
+          <img src="" alt="Sagar Tarang Cricket Association Logo">
+    </div> -->
+  <nav class="flex justify-around self-center w-1/2">
+      <router-link :to="{name: 'Home'}">Home</router-link>
+      <router-link :to="{name: 'About'}">About Us</router-link>
+      <router-link :to="{name: 'Contact'}">Contat Us</router-link>
+      <router-link :to="{name: 'Register'}">Register</router-link>
+  </nav>
+</header>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+.navbar {
+  background-color: #0F7173;
+}
+</style>
