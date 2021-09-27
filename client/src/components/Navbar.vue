7c1055653ae9a3eecb2801507ec984ba52fef4ac
Dilmohan <dilmohan11@gmail.com>
2021-09-27 13:49:58 +0530

    Navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,15 +1,31 @@
 <template>
 <header class="navbar h-24 flex justify-between text-white font-bold text-xl">
-
-  <img src="../assets/images/logo.png" alt="" class="">
-  <!-- <div class="">
-          <img src="" alt="Sagar Tarang Cricket Association Logo">
-    </div> -->
-  <nav class="flex justify-around self-center w-1/2">
-      <router-link :to="{name: 'Home'}">Home</router-link>
-      <router-link :to="{name: 'About'}">About Us</router-link>
-      <router-link :to="{name: 'Contact'}">Contat Us</router-link>
-      <router-link :to="{name: 'Register'}">Register</router-link>
+  
+  <router-link :to="{name: 'Home'}">
+    <img src="../assets/images/logo.png" alt="Sagar Tarang Association Logo" class="">
+  </router-link>
+  
+  <nav class="flex justify-around self-center w-1/2 ">
+      <router-link 
+        :to="{name: 'Home'}" 
+        class="hover:text-blue-300 transform transition duration-300 ease-linear">
+        Home
+      </router-link>
+      <router-link 
+        :to="{name: 'About'}" 
+        class="hover:text-blue-300 transform transition duration-300 ease-linear">
+        About Us
+      </router-link>
+      <router-link 
+        :to="{name: 'Contact'}" 
+        class="hover:text-blue-300 transform transition duration-300 ease-linear">
+        Contat Us
+      </router-link>
+      <router-link 
+        :to="{name: 'Register'}" 
+        class="hover:text-blue-300 transform transition duration-300 ease-linear">
+        Register
+      </router-link>
   </nav>
 </header>
 </template>
@@ -25,5 +41,9 @@ export default {
 <style>
 .navbar {
   background-color: #0F7173;
+}
+
+.router-link-exact-active{
+  color: #14EADD;
 }
 </style>
