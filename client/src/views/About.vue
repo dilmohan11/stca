3d0b637acc623be65f47cbfa3dac9eccc48299a2
Dilmohan <dilmohan11@gmail.com>
2021-10-01 13:09:13 +0530

    About and Home Page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <Herobox 
+      header="About Us"
+      quote="STCA is one of Versova’s premium underarm cricket league organizer since 15 years."
+    />
+    <section>
+      <div class="about_title flex flex-col ">
+            <h3 class="text-4xl text-center p-5">Our Story</h3>
+            <img src="../assets/images/tennisball.png" alt="Tennis Ball picture" class="w-12 h-12 mx-auto mb-5">
+        </div>
+        <div class="about_content flex gap-2 my-5">
+            <div class="para w-1/2 px-16">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis aliquam aliquet.
+                    Sed consectetur ultricies leo non imperdiet. Integer nibh mauris, finibus quis elit.
+                </p>
+            </div>
+            <div class="para w-1/2 px-16">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis aliquam aliquet.
+                    Sed consectetur ultricies leo non imperdiet. Integer nibh mauris, finibus quis elit.
+                </p>
+            </div>
+          </div>
+    </section>
+  </main>
 </template>
+
+<script>
+  import Herobox from '../components/Herobox.vue'
+  import Footer from '../components/Footer.vue'
+export default {
+  components: { Herobox, Footer }
+}
+</script>
+ 
+<style>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
-    
-  </div>
+  <main>
+    <section class="hero_section">
+      <div class="flex justify-center self-center pt-64 ">
+        <h1 class="text-6xl text-white ">
+        <span class="uppercase cricket_h1 font-bold">Cricket</span> is life!
+      </h1>
+      </div>
+      <div class="submit text-center">
+            <button class="text-lg px-4 py-4 font-semibold rounded-xl mt-5 bg-green-700 text-white hover:text-gray-50
+                            hover:bg-green-900 transform transition ease-linear duration-200">
+                Register Now
+            </button>
+        </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -13,3 +24,13 @@ export default {
   }
 }
 </script>
+
+<style>
+  .hero_section {
+    background: linear-gradient(to right, rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('../assets/images/hero_background.jpg');
+    height: 100vh;
+  }
+  .cricket_h1 {
+    color:#14EADD;
+  }
+</style>
